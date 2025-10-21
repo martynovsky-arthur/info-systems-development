fe0b6ddc3853720b033b88c11a3106dbc12bc3ea
Arthur Martynovsky <martynovsky@172.31.27.178-vpn.dhcp.yndx.net>
2025-10-22 02:35:51 +0300

    Oops

---
+++ lab2/model/__init__.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+from database.select import select_dict
+
+@dataclass
+class ResultInfo:
+    result: tuple
+    status: bool
+    err_message: str
+
+def model_route(_sql: str, user_input: dict | None = None) -> ResultInfo:
+    err_message = ''
+    result = select_dict(_sql, user_input)
+    if result:
+        return ResultInfo(result=result, status=True, err_message=err_message)
+    else:
+        err_message = 'Данные не получены'
+        return ResultInfo(result=result, status=False, err_message=err_message)
